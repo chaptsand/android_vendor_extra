fa2fb3fd5f1d3665c4771b5b75cfdfc484e968bd
chaptsand <chaptsand@gmail.com>
2022-04-12 18:50:23 +0800

    extra: Build ih8sn for gapps build

--- product.mk
+++ product.mk
@@ -7,4 +7,16 @@
 # Google Apps
 ifeq ($(WITH_GMS), true)
 $(call inherit-product, vendor/gms/products/gms.mk)
+
+# ih8sn
+PRODUCT_PACKAGES += ih8sn
+
+PRODUCT_COPY_FILES += \
+    vendor/extra/configs/ih8sn/ih8sn_umi.conf:/system/etc/ih8sn.conf
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/bin/ih8sn \
+    system/etc/ih8sn.conf \
+    system/etc/init/ih8sn.rc
+
 endif

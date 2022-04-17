705fd75f0c4f33a09eeda1ee6a5306daee1151eb
Vishalcj17 <vishalcj@aospa.co>
2022-04-17 18:33:54 +0800

    prebuilt: Ship GoogleCameraGo
    • Way better than Camera2, so override it as well
    
    Change-Id: I04a98f494233656d1719c4c3cbef409476fe7690

--- product.mk
+++ product.mk
@@ -4,6 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Camera
+PRODUCT_PACKAGES += \
+    GoogleCameraGo
+
+# Set compiler filter "verify" and disable AOT-compilation in dexpreopt
+RELAX_USES_LIBRARY_CHECK := true
+
 # Google Apps
 ifeq ($(WITH_GMS), true)
 $(call inherit-product, vendor/gms/products/gms.mk)

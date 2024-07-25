55bb49d8fe58b8a01ffa0d0a0c23ada1d6955e51
chaptsand <chaptsand@gmail.com>
2024-07-25 15:31:43 +0000

    here we go

--- product.mk
+++ product.mk
@@ -4,38 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Google Apps
-ifeq ($(WITH_GMS), true)
-$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
-
-# ih8sn
-PRODUCT_PACKAGES += ih8sn
-
-PRODUCT_COPY_FILES += \
-    vendor/extra/configs/ih8sn/ih8sn_umi.conf:/system/etc/ih8sn.conf
-
-PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
-    system/bin/ih8sn \
-    system/etc/ih8sn.conf \
-    system/etc/init/ih8sn.rc
-
-endif
-
-# Fonts
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,vendor/extra/prebuilts/fonts/,$(TARGET_COPY_OUT_PRODUCT)/fonts)
-
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/extra/overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay/common
-
 PRODUCT_PACKAGES += \
-    FontGoogleSansOverlay \
-    FontHarmonySansOverlay
-
-# ParanoidSense
-PRODUCT_PACKAGES += \
-    ParanoidSense \
-    FaceSenseOverlay
+    ParanoidSense
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
